282e75edcf9e3f7664f4f082a7c79aebb2b82594
Belen <belenjesuslp@gmail.com>
2023-03-03 18:09:40 -0300

    Haciendo los metodos comer y jugar

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
-public class Main extends PracticaJava{
+public class Main extends PracticaJava {
     public static void main(String[] args) {
 //    sumar(5,5.1);
 
+        //verdadero es que si, falso que no.
 
-    Tamagotchi belen=new Tamagotchi(0, false, true, true);
-    belen.comer();
-    belen.jugar();
-    Tamagotchi nico=new Tamagotchi(0, false, false, true);
-    nico.comer();
-    nico.jugar();
+        Tamagotchi belen = new Tamagotchi(80, 0, true, true, true);
+        belen.comer();
+        belen.jugar();
+        belen.mostrarEstado();
+
+        Tamagotchi nico = new Tamagotchi(0, 0, false, false, false);
+        nico.comer();
+        nico.jugar();
+        nico.mostrarEstado();
     }
 }
--- src/Tamagotchi.java
+++ src/Tamagotchi.java
@@ -1,65 +1,98 @@
 public class Tamagotchi {
+    int tiempo;
     int nivel;
     boolean aburrida;
     boolean hambre;
     boolean contenta;
 
     //constructor
-    public Tamagotchi(int nivel, boolean aburrida, boolean hambre, boolean contenta){
-        this.nivel= nivel;
-        this.aburrida=aburrida;
-        this.hambre= hambre;
-        this.contenta=contenta;
+    public Tamagotchi(int tiempo, int nivel, boolean aburrida, boolean hambre, boolean contenta) {
+        this.tiempo = tiempo;
+        this.nivel = nivel;
+        this.aburrida = aburrida;
+        this.hambre = hambre;
+        this.contenta = contenta;
     }
 
     //para modificar los datos
+    public void setTiempo(int tiempo) {
+        this.tiempo = tiempo;
+    }
+
     public void setNivel(int nivel) {
         this.nivel = nivel;
     }
+
     public void setAburrida(boolean aburrida) {
         this.aburrida = aburrida;
     }
+
     public void setHambre(boolean hambre) {
         this.hambre = hambre;
     }
+
     public void setContenta(boolean contenta) {
         this.contenta = contenta;
     }
 
     //para obtener los datos
+    public int getTiempo() {
+        return tiempo;
+    }
+
     public int getNivel() {
         return nivel;
     }
+
     public boolean isAburrida() {
         return aburrida;
     }
+
     public boolean isHambre() {
         return hambre;
     }
+
     public boolean isContenta() {
         return contenta;
     }
 
-    public void comer(){
-        if (this.hambre){
-            this.contenta=true;
-            System.out.println("Esta contenta :)");
-            ++this.nivel;
-            System.out.println(this.nivel);
-        }
-        else {
-            this.contenta=false;
-            System.out.println("No esta contenta, tiene hambre :(");
-            System.out.println(this.nivel);
-        }
+    public void mostrarEstado() {
+        System.out.println("Nivel: " + this.nivel);
+        System.out.println("¿Esta aburrida? " + this.aburrida);
+        System.out.println("¿Tiene hambre? " + this.hambre);
+        System.out.println("¿Esta contenta? " + this.contenta);
     }
-    public void jugar(){
-        if (this.aburrida)
-        this.aburrida=true;
-        System.out.println("No esta aburrieda :)");
-        ++this.nivel;
-        System.out.println(this.nivel);
 
+    //metodo comer
+    public void comer() {
+        if (this.hambre) {
+            this.contenta = true;
+            this.hambre = false;
+            ++this.nivel;
+        } else {
+            if (this.aburrida && this.tiempo > 80) {
+                this.contenta = true;
+            } else {
+                if (this.aburrida && this.tiempo <= 80) {
+                    mostrarEstado();
+                } else {
+                    this.contenta = false;
+                    System.out.println("No esta contenta, tiene hambre :(");
+                }
+            }
+        }
     }
-}
+
+        //metodo jugar
+        public void jugar () {
+            if (this.hambre = false) {
+                this.contenta = true;
+                System.out.println("Esta contenta :)");
+                this.nivel = this.nivel + 2;
+                this.aburrida = this.contenta;
+                mostrarEstado();
+
+            }
+        }
+    }
 
